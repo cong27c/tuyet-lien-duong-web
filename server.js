9823e597eee9c0fb0299480609417cae0ac135f6
Nguyễn Văn Công <nguyenvancongcbg27@hail.com>
2025-07-22 16:51:39 +0700

    sua port file server.js

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use((req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
